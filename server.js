e5d272c8d783f6afe737b951339c8ab77910ab2b
jacopo1395 <jack1395@hotmail.it>
2016-11-10 15:53:07 +0100

    firebase

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 var SpotifyWebApi = require('spotify-web-api-node');
 var firebase = require("firebase");
+var admin = require("firebase-admin");
 
 var client_id = 'c561f5078f1f400f98e008f23e26684d'; // Your client id
 var client_secret = 'fd67cd2602984213a46af856bca29541'; // Your secret
@@ -16,12 +17,19 @@ var spotifyApi = new SpotifyWebApi({
   redirectUri : redirect_uri
 });
 
-// firebase.initializeApp({
-//   serviceAccount: "path/to/serviceAccountCredentials.json",
-//   databaseURL: "https://triviamusic.firebaseio.com"
-// });
+var serviceAccount = require("./triviamusic-fd7d2-firebase-adminsdk-3ypir-3850f587fc.json");
 
-var categories =['rock',
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://triviamusic-fd7d2.firebaseio.com"
+});
+//https://firebase.google.com/docs/database/admin/save-data
+
+// Get a database reference to our blog
+var db = admin.database();
+var ref = db.ref("server/saving-data/fireblog");
+
+var categories ={"items":['rock',
                   'metal',
                   'pop',
                   'indie_alt',
@@ -33,7 +41,8 @@ var categories =['rock',
                   'jazz',
                   'blues',
                   'hiphop'
-                ];
+                ],
+              "status":"ok"};
 
 var playlists = {'items':{
                           'rock':
@@ -46,12 +55,23 @@ var playlists = {'items':{
                 'total': 3
                 };
 
+var startTime=[-1,0];
+var diff=[-1,0];
 
 var app = express();
 
 app.use(cookieParser());
 
 var get_access_token = function(callback){
+  diff = process.hrtime(startTime);
+  console.log(diff);
+  if(diff[0]==-1 || diff[0] >3000){
+    refresh_access_token(callback);
+  }
+  else callback(false);
+};
+
+var refresh_access_token = function(callback){
   // your application requests authorization
   var authOptions = {
     url: 'https://accounts.spotify.com/api/token',
@@ -63,7 +83,8 @@ var get_access_token = function(callback){
     },
     json: true
   };
-
+  startTime = process.hrtime();
+  console.log(startTime);
   request.post(authOptions, function(error, response, body) {
     if (!error && response.statusCode === 200) {
 
@@ -77,16 +98,30 @@ var get_access_token = function(callback){
   });
 };
 
-var refresh_access_token = function(){
-  // TODO: ....
-  var i;
-};
-
 
 
 app.get('/', function(req, res){
+  // startTime=process.hrtime();
   get_access_token(function(err){
-    var response;
+    if(err){
+      var response={};
+      response.status = "error";
+      res.setHeader('Content-Type', 'application/json');
+      res.send(response);
+      return;
+    }
+    var usersRef = ref.child("users");
+usersRef.set({
+  alanisawesome: {
+    date_of_birth: "June 23, 1912",
+    full_name: "Alan Turing"
+  },
+  gracehop: {
+    date_of_birth: "December 9, 1906",
+    full_name: "Grace Hopper"
+  }
+});
+    var response={};
     response.result="welcome!";
     response.status = "ok";
     res.setHeader('Content-Type', 'application/json');
@@ -104,7 +139,7 @@ app.get('/songs/:category', function(req, res){
   var id = playlists.items[req.params.category].id;
   get_access_token(function(err){
     if(err){
-      var response;
+      var response={};
       response.status = "error";
       res.setHeader('Content-Type', 'application/json');
       res.send(response);
@@ -133,7 +168,7 @@ app.get('/songs/:category', function(req, res){
         res.send(songs);
       }, function(err) {
         console.log('Something went wrong!', err);
-        var response;
+        var response={};
         response.status = "error";
         res.setHeader('Content-Type', 'application/json');
         res.send(response);
@@ -146,7 +181,7 @@ app.get('/songs/:category', function(req, res){
 app.get('/possibilities/:album_id', function(req,res){
     get_access_token(function(err){
       if(err){
-        var response;
+        var response={};
         response.status = "error";
         res.setHeader('Content-Type', 'application/json');
         res.send(response);
